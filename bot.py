c686485a465e61b3a658d725eb2b6c5a80866f61
SergeySergey1975 <t.s.a.1975@yandex.ru>
2026-09-25 12:31:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
- 8938508342:AAHVpGbmzjgko0FOk-uKF1kBSYd52jJlbOQ
+import requests
+
+token = "8938508342:AAHVpGbmzjgko0FOk-uKF1kBSYd52jJlbOQ"
+chat_id = "5114179830"
+
+tekst = "Доброе утро, Сергей! ☀️\nПора потратить 15 минут на Python. Ты отлично справляешься! 🚀"
+
+url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={tekst}"
+response = requests.get(url)
+
+if response.json().get('ok'):
+    print("Успешно отправлено!")
+else:
+    print("Ошибка:", response.json())
